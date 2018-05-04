f7844a3a014c595a6a09ee20e8abab602bc70142
sangyunxin <sangyunxin@gmail.com>
2018-05-04 14:27:25 +0800

    static file

--- Test/settings.py
+++ Test/settings.py
@@ -120,5 +120,5 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    "home/ethan//sites/demo.sangyx.cn/Test/static"
+    "home/ethan/sites/demo.sangyx.cn/Test/static"
 ]
