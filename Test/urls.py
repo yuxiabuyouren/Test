69d06a99d007dcacf6305370fbed3e8dbc00db11
sangyunxin <sangyunxin@gmail.com>
2018-05-04 13:34:55 +0800

    first release

--- Test/urls.py
+++ Test/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import index_view
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', index_view.index),
-]
+]+static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
